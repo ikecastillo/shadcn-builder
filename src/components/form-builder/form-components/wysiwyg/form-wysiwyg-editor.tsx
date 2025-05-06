1fd7d608d8e963f7026791ddf34d660a6855eeb3
Igor Duspara <igor.duspara@gmail.com>
2025-05-06 11:54:44 +0200

    fix: Replace breakpoint with container breakpoint in WYSIWYG Editor

--- src/components/form-builder/form-components/wysiwyg/form-wysiwyg-editor.tsx
+++ src/components/form-builder/form-components/wysiwyg/form-wysiwyg-editor.tsx
@@ -75,7 +75,7 @@ export const FormWysiwygEditor: React.FC<FormWysiwygEditorProps> = memo(
               ? node.attrs.level
               : this.options.levels[0];
             const classes: Record<number, string> = {
-              1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+              1: "scroll-m-20 text-4xl font-extrabold tracking-tight @5xl:text-5xl",
               2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
               3: "scroll-m-20 text-2xl font-semibold tracking-tight",
               4: "scroll-m-20 text-xl font-semibold tracking-tight",
